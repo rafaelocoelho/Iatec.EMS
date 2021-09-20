a117dc47667902181a87232cadc631e88957cddc
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-20 04:35:45 -0300

    Implementação para padronização no retorno de dados estabelecendo um padrão de comunicação

---
+++ src/Iatec.EMS.APi/Utilitis/Responses.cs
@@ -0,0 +1,58 @@
+﻿using Iatec.EMS.APi.ViewModels;
+using System.Collections.Generic;
+
+namespace Iatec.EMS.APi.Utilitis
+{
+    public static class Responses
+    {
+        public static ResultViewModel ApplicationErrorMessage()
+        {
+            return new ResultViewModel
+            {
+                Message = "Ocorreu algum erro interno na aplicação, por favor tente novamente",
+                Success = false,
+                Data = null
+            };
+        }
+
+        public static ResultViewModel ApplicationErrorMessage(string message)
+        {
+            return new ResultViewModel
+            {
+                Message = message,
+                Success = false,
+                Data = null
+            };
+        }
+
+        public static ResultViewModel DomainErroMessage(string message)
+        {
+            return new ResultViewModel
+            {
+                Message = message,
+                Success = false,
+                Data = null
+            };
+        }
+
+        public static ResultViewModel DomainErroMessage(string message, IReadOnlyCollection<string> errors)
+        {
+            return new ResultViewModel
+            {
+                Message = message,
+                Success = false,
+                Data = errors
+            };
+        }
+
+        public static ResultViewModel UnautorizedErrorMessage()
+        {
+            return new ResultViewModel
+            {
+                Message = "A combinação de login e senha está incoreta!",
+                Success = false,
+                Data = null
+            };
+        }
+    }
+}
---
+++ src/Iatec.EMS.APi/ViewModels/ResultViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Iatec.EMS.APi.ViewModels
+{
+    public class ResultViewModel
+    {
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
