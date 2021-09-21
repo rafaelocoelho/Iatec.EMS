47c0321c1524b974ad7caf9b5c913af47ae38956
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-21 02:14:35 -0300

    IMplementação da classe base para API

---
+++ src/Iatec.EMS.APi/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Iatec.EMS.APi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        protected readonly IMapper _mapper;
+
+        public BaseApiController() { }
+
+        public BaseApiController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
