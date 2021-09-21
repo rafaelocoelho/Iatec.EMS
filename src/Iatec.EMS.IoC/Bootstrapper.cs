fa5fb2a2995a837c1e4fe5e5152b016ceb09b4fd
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-21 02:09:49 -0300

    Implementação da assinatura dos serviços

---
+++ src/Iatec.EMS.IoC/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Iatec.EMS.Infra.Intefaces;
+using Iatec.EMS.Infra.Repositories;
+using Iatec.EMS.Services;
+using Iatec.EMS.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iatec.EMS.IoC
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            #region Repositories
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IEventParticipantRepository, EventParticipantRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            #endregion
+
+            #region Services
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IEventService, EventService>();
+            services.AddTransient<IEventParticipantService, EventParticipantService>();
+            #endregion
+
+            return services;
+        }
+    }
+}
