d5af8030fd1cdbad64debdd1ff2d0f4d685f4e0d
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-21 02:15:24 -0300

    Implementação das ViewModels

---
+++ src/Iatec.EMS.APi/ViewModels/Event/UpdateEventViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Iatec.EMS.Domain.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Iatec.EMS.APi.ViewModels
+{
+    public class UpdateEventViewModel
+    {
+        [Required(ErrorMessage = "O id não pode ser vazio")]
+        [Range(1, int.MaxValue, ErrorMessage = "o id não pode ser menor que 1.")]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "O nome não pode ser vazio")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "A descrição não pode ser vazia")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date), Required(ErrorMessage = "A data não pode ser vazia")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "O local não pode ser vazio")]
+        public string Local { get; set; }
+
+        [Required(ErrorMessage = "O tipo não pode ser vazio")]
+        public EventTypeEnum Type { get; set; }
+    }
+}
---
+++ src/Iatec.EMS.APi/ViewModels/User/UpdateUserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Iatec.EMS.Domain.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Iatec.EMS.APi.ViewModels
+{
+    public class UpdateUserViewModel
+    {
+        [Required(ErrorMessage = "O id não pode ser vazio")]
+        [Range(1, int.MaxValue, ErrorMessage = "o id não pode ser menor que 1.")]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "O nome não pode ser vazio")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O email não pode ser vazia")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), Required(ErrorMessage = "A senha não pode ser vazia")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Date), Required(ErrorMessage = "A data de nascimento não pode ser vazia")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "O gênero não pode ser vazio")]
+        public GenderEnum Gender { get; set; }
+    }
+}
