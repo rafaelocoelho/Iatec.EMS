ba69010ce505540b07cfb40fd808aafc6bf014ff
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-21 02:10:40 -0300

    Impliementação do contexto para acesso a base de dados

---
+++ src/Iatec.EMS.Infra/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Iatec.EMS.Infra.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iatec.EMS.Infra
+{
+    public static class Bootstrapper
+    {
+        public static void AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnetion"),
+                    assembly => assembly.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        }
+    }
+}
---
+++ src/Iatec.EMS.Infra/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Iatec.EMS.Domain.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iatec.EMS.Infra.Contexts
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EventMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new EventParticipantMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
