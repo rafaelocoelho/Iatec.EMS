a1beb13b56787a840bf7461bcbbe5f09b679fc7c
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-29 00:43:48 -0300

    Implementação automapper por meio de mapping profile, incluindos Entities, DTOs e ViewModels.

---
+++ src/Iatec.EMS.APi/Infra/AutoMapper/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Iatec.EMS.Api.ViewModels;
+using Iatec.EMS.Domain.Entities;
+using Iatec.EMS.Services.DTOs;
+
+namespace Iatec.EMS.Api.Infra.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region User
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<CreateUserViewModel, UserDTO>().ReverseMap();
+            CreateMap<UpdateUserViewModel, UserDTO>().ReverseMap();
+            CreateMap<LoginUserViewModel, UserDTO>().ReverseMap();
+            #endregion
+
+            #region Event
+            CreateMap<Event, EventDTO>().ReverseMap();
+            CreateMap<CreateEventViewModel, EventDTO>().ReverseMap();
+            CreateMap<UpdateEventViewModel, EventDTO>().ReverseMap();
+            #endregion
+        }
+    }
+}
---
+++ src/Iatec.EMS.APi/Infra/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Iatec.EMS.Api.Infra.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iatec.EMS.Api.Infra.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddAPIServices(this IServiceCollection services)
+        {
+            #region Mapper  
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfiguration.CreateMapper();
+
+            services.AddSingleton(mapper);
+            #endregion
+
+            return services;
+        }
+    }
+}
