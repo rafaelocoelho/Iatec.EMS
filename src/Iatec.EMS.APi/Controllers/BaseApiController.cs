f8f3c3c0e94e83e576c83d2096954b4c165d6ca0
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-29 00:47:29 -0300

    Implementação de clase base para api

---
+++ src/Iatec.EMS.APi/Controllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Iatec.EMS.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public abstract class BaseApiController : ControllerBase
+    {
+        protected readonly IMapper _mapper;
+        protected const string _version = "api/v1/";
+
+        public BaseApiController(
+            IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public BaseApiController()
+        {
+        }
+    }
+}
