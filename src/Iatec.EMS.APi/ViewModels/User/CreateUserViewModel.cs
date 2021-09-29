3fe901aca46461d607c574aa4edd68ce25e8e09c
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-29 00:49:11 -0300

    Implementação de alguns ajustes nas ViewModels.

--- src/Iatec.EMS.APi/ViewModels/User/CreateUserViewModel.cs
+++ src/Iatec.EMS.APi/ViewModels/User/CreateUserViewModel.cs
@@ -6,19 +6,19 @@ namespace Iatec.EMS.Api.ViewModels
 {
     public class CreateUserViewModel
     {
-        [Required(ErrorMessage = "O nome não pode ser vazio")]
+        [Required(ErrorMessage = "O nome não pode ser vazio.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "O email não pode ser vazia")]
+        [Required(ErrorMessage = "O email não pode ser vazia."), EmailAddress(ErrorMessage = "O email não é válido.")]
         public string Email { get; set; }
 
-        [DataType(DataType.Password), Required(ErrorMessage = "A senha não pode ser vazia")]
+        [DataType(DataType.Password), Required(ErrorMessage = "A senha não pode ser vazia.")]
         public string Password { get; set; }
 
-        [DataType(DataType.Date), Required(ErrorMessage = "A data de nascimento não pode ser vazia")]
+        [DataType(DataType.Date), Required(ErrorMessage = "A data de nascimento não pode ser vazia.")]
         public DateTime Birthday { get; set; }
 
-        [Required(ErrorMessage = "O gênero não pode ser vazio")]
+        [Required(ErrorMessage = "O gênero não pode ser vazio.")]
         public GenderEnum Gender { get; set; }
     }
 }
---
+++ src/Iatec.EMS.APi/ViewModels/User/LoginUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Iatec.EMS.Api.ViewModels
+{
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "O email não pode ser vazia."), EmailAddress(ErrorMessage = "O email não é válido.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), Required(ErrorMessage = "A senha não pode ser vazia.")]
+        public string Password { get; set; }
+    }
+}
