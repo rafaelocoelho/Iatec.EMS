c63ea1db75ebda49fa46adf906a47b178acefc93
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-30 12:19:46 -0300

    Implementação de alguns ajustes, incluindo criação do endpoint search para pesquisa de eventos.

---
+++ src/Iatec.EMS.APi/ViewModels/Event/SearchEventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Iatec.EMS.Api.ViewModels
+{
+    public class SearchEventViewModel
+    {
+        public string Name { get; set; }
+
+        public DateTime InitialDate { get; set; }
+
+        public DateTime FinalDate { get; set; }
+    }
+}
