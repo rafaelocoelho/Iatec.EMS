8bf5cb00ad47dcb13de2b60de569392372253446
Rafael Oliveira Coelho <rafael.coelho@rte.com.br>
2021-09-30 12:40:43 -0300

    Criação de migration para alterar tamanho do campo password da entidade user.

--- src/Iatec.EMS.Infra/Migrations/20210930030731_UpdatePasswordLenght.Designer.cs
+++ src/Iatec.EMS.Infra/Migrations/20210930030731_UpdatePasswordLenght.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Iatec.EMS.Infra.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Iatec.EMS.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210930030731_UpdatePasswordLenght")]
+    partial class UpdatePasswordLenght
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Iatec.EMS.Infra/Migrations/20210930030731_UpdatePasswordLenght.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Iatec.EMS.Infra.Migrations
+{
+    public partial class UpdatePasswordLenght : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ems_eveprt_ems_event_EventId1",
+                table: "ems_eveprt");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ems_eveprt_ems_user_UserId1",
+                table: "ems_eveprt");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ems_eveprt_EventId1",
+                table: "ems_eveprt");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ems_eveprt_UserId1",
+                table: "ems_eveprt");
+
+            migrationBuilder.DropColumn(
+                name: "EventId1",
+                table: "ems_eveprt");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "ems_eveprt");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "usr_passwd",
+                table: "ems_user",
+                type: "VARCHAR(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(10)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "usr_passwd",
+                table: "ems_user",
+                type: "VARCHAR(10)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)");
+
+            migrationBuilder.AddColumn<long>(
+                name: "EventId1",
+                table: "ems_eveprt",
+                type: "BIGINT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId1",
+                table: "ems_eveprt",
+                type: "BIGINT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ems_eveprt_EventId1",
+                table: "ems_eveprt",
+                column: "EventId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ems_eveprt_UserId1",
+                table: "ems_eveprt",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ems_eveprt_ems_event_EventId1",
+                table: "ems_eveprt",
+                column: "EventId1",
+                principalTable: "ems_event",
+                principalColumn: "evt_id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ems_eveprt_ems_user_UserId1",
+                table: "ems_eveprt",
+                column: "UserId1",
+                principalTable: "ems_user",
+                principalColumn: "usr_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
